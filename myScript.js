32f1538aa44790b1140124285e4be09e57fd817a
MahathiReddyVakeel <vklrdy.m9@gmail.com>
2024-06-26 18:17:21 -0400

    Create myScript.js

---
+++ myScript.js
@@ -0,0 +1,14 @@
+						                                                      <!--demo-->
+
+function showSummary(){
+document.getElementById("demo").innerHTML = "I am an IT professional with over two years of experience in software development and teaching assistance, specializing in Java, Python, C++, and SQL. Proficient in AWS and Microsoft Azure, I have successfully managed complex ETL processes and developed high-accuracy predictive models. At TCS, I maintained high uptime for automated processes by efficiently resolving API job failures. As a Graduate Teaching Assistant at the University of Cincinnati, I supported students in various technical subjects, earning the Outstanding Student of MSIT Award.I developed a Java-based Customer Experience Enhancement Platform, integrating advanced analytics and real-time data processing. Known for my proactive problem-solving and continuous improvement approach, I drive significant system efficiencies. Passionate about fostering innovation and effective team collaboration, I leverage technology to solve complex problems and achieve organizational success.";
+}
+
+					                                                      	<!--demo1-->
+function showResume(){
+document.getElementById("demo1").style.display="block";
+}
+
+						                                                      <!--demo2-->
+showSummary();
+showResume();
